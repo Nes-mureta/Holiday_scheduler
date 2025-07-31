52f9544892d581bde6e2eca809c49aebf37b1521
Nesbit <nesbitmuretar@gmail.com>
2025-08-01 00:38:49 +0300

    fix: allow all hosts in ALLOWED_HOSTS for development

--- Holiday_timetable/settings.py
+++ Holiday_timetable/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-_a@1nkm3f^6e!2(at&&jab)tsu+s%2qmr3=n8f81wy)-ce$=h(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
